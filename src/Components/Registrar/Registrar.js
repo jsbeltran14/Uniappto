dd0fe703489136d9ff2d137bef008b9083eea889
SEBASTIAN BELTRAN MORALES <js.beltran14@uniandes.edu.co>
2021-11-20 17:03:09 -0500

    registrarse

--- src/Components/Registrar/Registrar.js
+++ src/Components/Registrar/Registrar.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input } from "../Input/Input";
 import { Link } from "react-router-dom";
+import "./styles.css";
 
 export const Registrar = () => {
 
-    function handleChange(event) {
-        if (event.target.name === "email") {
-        } else {
-        }
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const apiOrigin = "http://localhost:3001/api";
+
+  function handleChange(event) {
+    if (event.target.name === "name") {
+      setName(event.target.value);
+    }
+    else if (event.target.name === "email") {
+      setEmail(event.target.value);
+    } 
+    else if (event.target.name === "password") {
+      setPassword(event.target.value);
+    }
+  }
+
+  const postUser = async () => {
+    try {
+      const resp = await fetch(`${apiOrigin}/users`, {
+        method: "POST",
+        body: JSON.stringify({ username:name , email: email, password: password
+                              , age:20, career:"none", pic_url:"none", university:"none", semester:"none" }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return resp.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSubmit = async () => {
+    const account = { name, email, password };
+    if (account) {
+      const data = await postUser();
+      if (data.success === true) {
+        console.log("usuario creado");
       }
+    }
+  };
 
     return (
         <div className="container__login">
@@ -19,10 +58,10 @@ export const Registrar = () => {
       <h2>Nombre</h2>
         <Input
           attribute={{
-            id: "email",
-            name: "email",
+            id: "name",
+            name: "name",
             type: "text",
-            placeholder: "Email o número telefónico",
+            placeholder: "Nombre",
           }}
           handleChange={handleChange}
         />
@@ -39,8 +78,8 @@ export const Registrar = () => {
         <h2>Contraseña</h2>
         <Input
           attribute={{
-            id: "contraseña",
-            name: "contraseña",
+            id: "password",
+            name: "password",
             type: "password",
             placeholder: "Contraseña",
           }}
@@ -49,18 +88,17 @@ export const Registrar = () => {
         <h2>Repetir Contraseña</h2>
         <Input
           attribute={{
-            id: "contraseña",
-            name: "contraseña",
+            id: "password2",
+            name: "password2",
             type: "password",
             placeholder: "Contraseña",
           }}
           handleChange={handleChange}
         />
         <Link to="/">
-          <button  className="login__button">
+          <button onClick={handleSubmit} className="login__button">
             Registrarse
           </button>
-          {/* TODO: Just authenticated users*/}
         </Link>
       </div>
     </div>
